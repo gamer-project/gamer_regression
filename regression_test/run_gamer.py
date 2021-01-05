39e8da8da97f5ebb94f5f1b35200c43fcc7719db
xuanweishan <xuanweishan@gmail.com>
2021-01-05 18:03:28 +0800

    Compile and run gamer by python scrip.

---
+++ regression_test/run_gamer.py
@@ -0,0 +1,89 @@
+import logging
+import os
+import subprocess
+import pandas as pd
+import shutil as st
+
+config_path = 'make_config'
+gamer_abs_path = '/work1/xuanshan/gamer_test'
+input_folder = gamer_abs_path + '/example/test_problem/Hydro/Riemann'
+
+test_cmds = [['sed','-i','s/#SIMU_OPTION += -DMODEL=HYDRO/SIMU_OPTION += -DMODEL=HYDRO/g','Makefile'],\
+	['sed','-i','s/SIMU_OPTION += -DGRAVITY/#SIMU_OPTION += -DGRAVITY/g','Makefile'],\
+	['sed','-i','s/SIMU_OPTION += -DPARTICLE/#SIMU_OPTION += -DPARTICLE/g','Makefile'],\
+	['sed','-i','s/SIMU_OPTION += -DSUPPORT_GRACKLE/#SIMU_OPTION += -DSUPPORT_GRACKLE/g','Makefile'],\
+	['sed','-i','s/SIMU_OPTION += -DGPU/#SIMU_OPTION += -DGPU/g','Makefile'],\
+	['sed','-i','s/#SIMU_OPTION += -DSERIAL/SIMU_OPTION += -DSERIAL/g','Makefile'],\
+	['sed','-i','s/SIMU_OPTION += -DLOAD_BALANCE=HILBERT/#SIMU_OPTION += -DLOAD_BALANCE=HILBERT/g','Makefile'],\
+	['sed','-i','s/SIMU_OPTION += -DOPENMP/#SIMU_OPTION += -DOPENMP/g','Makefile']]
+
+test_config = {'Enable':['MODEL=HYDRO','SERIAL'],\
+	       'Disable':['GRAVITY','PARTICLE','SUPPORT_GRACKLE','GPU','LOAD_BALANCE=HILBERT','OPENMP']}
+
+def get_config(config_path):
+	config_file= open(config_path)
+	config_text = config_file.readlines()
+
+	config = {'Enable':[],'Disable':[]}
+	for line in config_text:
+		settings = line.rstrip().split('\t')
+		if settings[0] == 'Enable':
+			config['Enable'] = settings[1].split(',')
+		elif settings[0] == 'Disable':
+			config['Disable'] = settings[1].split(',')
+	return config
+
+#Edit gamer configuration settings
+def make(config):
+#	get commands to midify Makefile.
+	cmds = []
+#	add enable options
+	for enable_option in config['Enable']:
+		cmds.append(['sed','-i','s/#SIMU_OPTION += -D%s/SIMU_OPTION += -D%s/g'%(enable_option,enable_option),'Makefile'])
+#	add disable options
+	for disable_option in config['Disable']:
+		cmds.append(['sed','-i','s/SIMU_OPTION += -D%s/#SIMU_OPTION += -D%s/g'%(disable_option,disable_option),'Makefile'])
+#	Back up and modify Makefile
+	current_path = os.getcwd()
+	os.chdir(gamer_abs_pathi + '/src')
+	subprocess.check_call(['cp', 'Makefile', 'Makefile.origin'])
+#	Makefile configuration
+	try:
+		for cmd in cmds:
+			subprocess.check_call(cmd)
+	except subprocess.CalledProcessError , err:
+		print 'err', err.cmd
+#	Make
+	try:
+		subprocess.check_call(['make','clean'])
+		subprocess.check_call(['make','-j'])
+	except subprocess.CalledProcessError , err:
+		print 'err', err.cmd
+#	Repair Makefile
+	subprocess.check_call(['cp', 'Makefile.origin', 'Makefile'])
+	subprocess.check_call(['rm', 'Makefile.origin'])
+	return 0
+
+def run(file_folder):
+	run_directory = gamer_abs_path + '/bin'
+	os.chdir(run_directory)
+	test_name = file_folder.split('/')[-1]
+	st.copytree(file_folder,test_name)
+	os.chdir(test_name)
+	st.copy('../gamer','.')
+	try:
+		subprocess.check_call(['./gamer'])
+	except subprocess.CalledProcessErro , err:
+		print 'err', err.cmd
+
+	return 0
+
+def analyze():
+
+	return 0
+
+#seirpt self test
+if __name__ == '__main__':
+#	config = get_config(config_path)
+#	make(config)
+	run(input_folder)
